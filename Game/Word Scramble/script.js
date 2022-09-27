9fd64431a417a161b7b9937d1d8877cffa0b0bdb
Eren İncedayı <erenincedayi9065@gmail.com>
2022-09-27 16:34:56 +0300

    World Scramble

---
+++ Game/Word Scramble/script.js
@@ -0,0 +1,59 @@
+const wordText = document.querySelector(".word"),
+hintText = document.querySelector(".hint span"),
+timeText = document.querySelector(".time b"),
+inputField = document.querySelector("input"),
+refreshBtn = document.querySelector(".refresh-word"),
+checkBtn = document.querySelector(".check-word");
+
+let correctWord, timer;
+
+const initTimer = maxTime =>
+{
+    clearInterval(timer);
+
+    timer = setInterval(() =>
+    {
+        if (maxTime > 0) 
+        {
+            maxTime--;
+            return timeText.innerText = maxTime;
+        }
+        clearInterval(timer);
+        alert(`Zaman Doldu! Cevap ${correctWord.toUpperCase()} olacaktı. `);
+        initGame();
+
+    }, 1000);
+}
+const initGame = () =>
+{
+    initTimer(30);
+    let randomObj = words[Math.floor(Math.random() * words.length)];
+    let wordArray = randomObj.word.split("");
+
+    for (let i = wordArray.length - 1; i > 0; i--)
+    {
+        let j = Math.floor(Math.random() * (i + 1));
+        [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]];
+    }
+
+    wordText.innerText = wordArray.join("");
+    hintText.innerText = randomObj.hint;
+    correctWord = randomObj.word.toLocaleLowerCase();
+    inputField.value = "";
+    inputField.setAttribute("maxlength", correctWord.length);
+
+}
+
+initGame();
+
+const checkWord = () => 
+{
+    let userWord = inputField.value.toLocaleLowerCase();
+    if(!userWord) return alert(`Boş bir değer göndermeye çalışıyorsunuz. Lütfen gerçerli bir değer girin!`);
+    if(userWord !== correctWord) return alert(`Oops! ${userWord} doğru bir kelime değil!`);
+    alert(`Tebrikler! ${userWord.toLocaleUpperCase()} doğru tahmin!`);
+    initGame();
+}
+
+refreshBtn.addEventListener("click", initGame);
+checkBtn.addEventListener("click", checkWord);
