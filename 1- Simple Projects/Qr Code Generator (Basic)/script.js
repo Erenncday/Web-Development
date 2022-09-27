80740359900ffac01758b2f8eab59bcd529f2c2c
Eren İncedayı <erenincedayi9065@gmail.com>
2022-09-27 16:49:43 +0300

    Qr Code Generator Uploaded

---
+++ 1- Simple Projects/Qr Code Generator (Basic)/script.js
@@ -0,0 +1,32 @@
+const wrapper = document.querySelector(".wrapper"),
+qrInput = wrapper.querySelector(".form input"),
+generateBtn = wrapper.querySelector(".form button"),
+qrImg = wrapper.querySelector(".qr-code img");
+
+generateBtn.addEventListener("click", () =>
+{
+    let qrValue = qrInput.value;
+    if(!qrValue) return;
+
+    generateBtn.innerText = "Generating QR Code...";
+
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qrValue}`;
+    qrImg.addEventListener("load", ()=>
+    {
+        wrapper.classList.add("active");
+        generateBtn.style.backgroundColor = "black";
+        generateBtn.style.color = "orange";
+        generateBtn.innerText = "QR Code Generated";
+
+    })
+    
+});
+
+
+qrInput.addEventListener("keyup", ()=>
+{
+    if(!qrInput.value)
+    {
+        wrapper.classList.remove("active");
+    }
+})
